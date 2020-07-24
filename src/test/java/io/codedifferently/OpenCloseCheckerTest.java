df786516258741ec60a6b1d3cbf21f77c73ee6d2
beastlyCoder <imaaronacs@gmail.com>
2020-07-24 11:27:34 -0700

    doneeee

--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,61 @@
 package io.codedifferently;
 
-public class OpenCloseCheckerTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class OpenCloseCheckerTest
+{
+
+    @Test
+    public void testQuotes()
+    {
+        String s = "bobby\"tarantino\"hehehehe";
+
+        boolean expected = true;
+        System.out.println(s);
+
+        Assert.assertEquals(expected, new OpenCloseChecker().simpleChecker(s));
+    }
+
+    @Test
+    public void testSingleQuotes()
+    {
+        String s = "bobby\'tarantino\'hehehehe";
+
+        boolean expected = true;
+        System.out.println(s);
+
+        Assert.assertEquals(expected, new OpenCloseChecker().simpleChecker(s));
+    }
+
+    @Test
+    public void testParenth()
+    {
+        String s = "(bobby(tarantino)hehehehe)";
+
+        boolean expected = true;
+        System.out.println(s);
+
+        Assert.assertEquals(expected, new OpenCloseChecker().simpleChecker(s));
+    }
+
+    @Test
+    public void testTriangle()
+    {
+        String s = "<ehhehehehe>";
+        boolean expected = true;
+        System.out.println(s);
+
+        Assert.assertEquals(expected, new OpenCloseChecker().simpleChecker(s));
+    }
+
+    @Test
+    public void testCurly()
+    {
+        String s = "{}{}";
+        boolean expected = true;
+        System.out.println(s);
+
+        Assert.assertEquals(expected, new OpenCloseChecker().simpleChecker(s));
+    }
 }
