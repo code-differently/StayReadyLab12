0af140f993f8faafa7e8debf07e49a8a1c5bca12
stampedsteve <67400828+stampedsteve@users.noreply.github.com>
2020-07-24 12:38:20 -0400

    completed lab 12

--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,113 @@
 package io.codedifferently;
 
-public class OpenCloseCheckerTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class OpenCloseCheckerTest {
+    @Test
+    public void ParanthCheck(){
+        //Given
+        String str = "sosa )()()";
+        OpenCloseChecker OC = new OpenCloseChecker();
+        //Then
+        boolean expected = false;
+        boolean actual = OC.parenthCheck(str);
+        //When
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ParanthCheck2(){
+        //Given
+        String str = "sosa ()()";
+        OpenCloseChecker OC = new OpenCloseChecker();
+        //Then
+        boolean expected = true;
+        boolean actual = OC.parenthCheck(str);
+        //When
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void anchorCheck(){
+        //Given
+        String str = "sosa <><>";
+        OpenCloseChecker OC = new OpenCloseChecker();
+        //Then
+        boolean expected = true;
+        boolean actual = OC.anchorCheck(str);
+        //When
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void staightyCheck(){
+        //Given
+        String str = "sosa [][]";
+        OpenCloseChecker OC = new OpenCloseChecker();
+        //Then
+        boolean expected = true;
+        boolean actual = OC.straightyCheck(str);
+        //When
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void curlyCheck(){
+        //Given
+        String str = "sosa {}{}";
+        OpenCloseChecker OC = new OpenCloseChecker();
+        //Then
+        boolean expected = true;
+        boolean actual = OC.curlyCheck(str);
+        //When
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void quoteCheck(){
+        //Given
+        String str = "sosa \"\"\"";
+        OpenCloseChecker OC = new OpenCloseChecker();
+        //Then
+        boolean expected = false;
+        boolean actual = OC.quoteCheck(str);
+        //When
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void singlyQuoteCheck(){
+        //Given
+        String str = "sosa ''";
+        OpenCloseChecker OC = new OpenCloseChecker();
+        //Then
+        boolean expected = true;
+        boolean actual = OC.singlyQuoteCheck(str);
+        //When
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void AllOPCheck(){
+        //Given
+        String str = "sosa >()[]{}\'\'\"\"";
+        OpenCloseChecker OC = new OpenCloseChecker();
+        //Then
+        boolean expected = false;
+        boolean actual = OC.allOPCheck(str);
+        //When
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void AllOPCheck2(){
+        //Given
+        String str = "sosa <>()[]{}\'\'\"\"";
+        OpenCloseChecker OC = new OpenCloseChecker();
+        //Then
+        boolean expected = true;
+        boolean actual = OC.allOPCheck(str);
+        //When
+        Assert.assertEquals(expected, actual);
+    }
 }
