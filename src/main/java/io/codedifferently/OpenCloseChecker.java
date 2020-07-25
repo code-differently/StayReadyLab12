5f42db677738c06cc67c6e9813bb3459694dcac7
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-25 01:33:44 -0400

    Added a isBalancedv1() and  isBalancedv2()

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,91 @@
 package io.codedifferently;
 
+import java.util.Stack;
+
+/**
+ * LOGIC: 1. All opening symbols are pushed to the stack.
+ *        2. If come across closing symbol, check top of stack
+ *        for the most recent opening symbol.
+ *        3. If symbols don't coorespond to each other (match),
+ *        then RETURN FALSE.
+ *        4. If you get to the end of sntnc and the stack isn't empty,
+ *        then that means there's an opening symbol that wasn't matched,
+ *        so RETURN FALSE.
+ */
 public class OpenCloseChecker {
+
+    /**
+     *
+     * @param sntnc - string to check if balanced
+     * @return
+     *
+     */
+    public static boolean isBalancedv1(String sntnc){
+        boolean isBalanced = true;
+        Stack<Character> symbols = new Stack<Character>();  // create new stack to store the opening symbols
+        for (Character currChar : sntnc.toCharArray()){     // iter-ing thru every char in sntnc
+            if (currChar.equals('(')){                      // if currChar is opening symbol, push to symbols
+                symbols.push(currChar);
+            }
+            else if (currChar.equals(')')){                 // if currChar is closing symbol,
+                if (!symbols.pop().equals('(')){            // yet the most recent opening symbol doesnt match, set isBalanced to false
+                    isBalanced = false;
+                }
+            }
+        }
+        if (!symbols.isEmpty()){ isBalanced = false; }            // if symbols isnt empty, set isBalanced to false
+        return isBalanced;
+    }
+
+    /**
+     *
+     * @param sntnc - string to check if balanced
+     * @return
+     *
+     * version 2 of isBalanced expands on the functionality
+     * to check that all opening characters have a closing one.
+     *
+     * Characters  include: () {} [] <> "" ''
+     *
+     *
+     */
+    public static boolean isBalancedv2(String sntnc){
+        boolean isBalanced = true;
+        Stack<Character> symbols = new Stack<Character>();  // create new stack to store the opening symbols
+        for (Character currChar : sntnc.toCharArray()){     // iter-ing thru every char in sntnc
+            if (currChar.equals('(')){                      // if currChar is opening symbol, push to symbols
+                symbols.push(currChar);
+            } else if (currChar.equals(')')){                 // if currChar is closing symbol,
+                if (!symbols.pop().equals('(')){            // yet the most recent opening symbol doesnt match, set isBalanced to false
+                    isBalanced = false;
+                }
+            } else if (currChar.equals('{')){
+                symbols.push(currChar);
+            } else if (currChar.equals('}')){
+                if (!symbols.pop().equals('{')){
+                    isBalanced = false;
+                }
+            } else if (currChar.equals('[')){
+                symbols.push(currChar);
+            } else if (currChar.equals(']')){
+                if (!symbols.pop().equals('[')){
+                    isBalanced = false;
+                }
+            } else if (currChar.equals('"') && !symbols.contains('"')){     // if currChar is quotation and symbol DOESN'T have a quotation already
+                symbols.push(currChar);
+            } else if (currChar.equals('"') && symbols.contains('"')){      // if currChar is quotation and symbol DOES have a quotation already
+                if (!symbols.pop().equals('"'))
+                    isBalanced = false;
+            } else if (currChar.equals('\'') && !symbols.contains('\'')){     // if currChar is quotation and symbol DOESN'T have a quotation already
+                symbols.push(currChar);
+            } else if (currChar.equals('\'') && symbols.contains('\'')){      // if currChar is quotation and symbol DOES have a quotation already
+                if (!symbols.pop().equals('\'')){
+                    isBalanced = false;
+                }
+            }
+        }
+        if (!symbols.isEmpty()){ isBalanced = false; }
+        return isBalanced;
+    }
+
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,109 @@
 package io.codedifferently;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.EmptyStackException;
+
 public class OpenCloseCheckerTest {
 
+    @Test
+    public void isBalancedv1_Test1(){
+        // Given
+        String eyd1 = "(e)(y)(d)";
+        String eyd2 = "(EMBRACE)(YO)(DOPENESS)(!)"; // you already know ¯\_(ツ)_/¯
+        String noParentheses = "Let's have a jam session!"; //srsly tho, i'm ALWAYS in the mood to vibe out with people
+        String randStr = "(()())";
+        String nothing = "";
+
+        // When
+        boolean actual1 = OpenCloseChecker.isBalancedv1(noParentheses);
+        boolean actual2 = OpenCloseChecker.isBalancedv1(nothing);
+        boolean actual3 = OpenCloseChecker.isBalancedv1(eyd1);
+        boolean actual4 = OpenCloseChecker.isBalancedv1(eyd2);
+        boolean actual5 = OpenCloseChecker.isBalancedv1(randStr);
+
+        // Then
+        Assert.assertTrue(actual1);
+        Assert.assertTrue(actual2);
+        Assert.assertTrue(actual3);
+        Assert.assertTrue(actual4);
+        Assert.assertTrue(actual5);
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void isBalancedv1_Test2(){
+        // Given
+        String randStr1 = "(shama())lama)";     // not balanced
+        String randStr2 = "(()()))";            // not balanced
+        String randStr3 = "((()())";            // not balanced
+        String test1 = ")";
+        String test2 = "(";
+
+        // When
+        boolean actual1 = OpenCloseChecker.isBalancedv1(randStr1);
+        boolean actual2 = OpenCloseChecker.isBalancedv1(randStr2);
+        boolean actual3 = OpenCloseChecker.isBalancedv1(randStr3);
+        boolean actual4 = OpenCloseChecker.isBalancedv1(test1);
+        boolean actual5 = OpenCloseChecker.isBalancedv1(test2);
+
+        // Then
+        Assert.assertFalse(actual1);
+        Assert.assertFalse(actual2);
+        Assert.assertFalse(actual3);
+        Assert.assertFalse(actual4);
+        Assert.assertFalse(actual5);
+    }
+
+    @Test
+    public void isBalancedv2_Test1(){
+        // Given
+        String eyd1 = "[(e)(y)\"d\"]";
+        String eyd2 = "'[EMBRACE]{YO}\"DOPENESS\"(!)'"; // you already know ¯\_(ツ)_/¯
+        String randStr1 = "('jam session!')";
+        String randStr2 = "()";
+        String randStr3 = "[({[]''})]";
+        String randStr4 = "''";
+        String randStr5 = "[]{}";
+
+        // When
+        boolean actual1 = OpenCloseChecker.isBalancedv2(randStr1);
+        boolean actual2 = OpenCloseChecker.isBalancedv2(randStr4);
+        boolean actual3 = OpenCloseChecker.isBalancedv2(eyd1);
+        boolean actual4 = OpenCloseChecker.isBalancedv2(eyd2);
+        boolean actual5 = OpenCloseChecker.isBalancedv2(randStr2);
+        boolean actual6 = OpenCloseChecker.isBalancedv2(randStr5);
+
+        // Then
+        Assert.assertTrue(actual1);
+        Assert.assertTrue(actual2);
+        Assert.assertTrue(actual3);
+        Assert.assertTrue(actual4);
+        Assert.assertTrue(actual5);
+        Assert.assertTrue(actual6);
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void isBalancedv2_Test2(){
+        // Given
+        String randStr1 = "(sh[])ma}";     // not balanced
+        String randStr2 = "({}[])'";            // not balanced
+        String randStr3 = "(\")";            // not balanced
+        String test1 = ")";
+        String test2 = "{";
+
+        // When
+        boolean actual1 = OpenCloseChecker.isBalancedv1(randStr1);
+        boolean actual2 = OpenCloseChecker.isBalancedv1(randStr2);
+        boolean actual3 = OpenCloseChecker.isBalancedv1(randStr3);
+        boolean actual4 = OpenCloseChecker.isBalancedv1(test1);
+        boolean actual5 = OpenCloseChecker.isBalancedv1(test2);
+
+        // Then
+        Assert.assertFalse(actual1);
+        Assert.assertFalse(actual2);
+        Assert.assertFalse(actual3);
+        Assert.assertFalse(actual4);
+        Assert.assertFalse(actual5);
+    }
 }
