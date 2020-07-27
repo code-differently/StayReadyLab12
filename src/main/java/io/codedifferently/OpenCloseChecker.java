33a5cb05d4e0090a42289b73c597f22092f98f09
johndacosmo <jpali@bellsouth.net>
2020-07-26 22:17:21 -0400

    Lab finished

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,64 @@
 package io.codedifferently;
 
+import java.util.HashMap;
+
 public class OpenCloseChecker {
+
+    private Character openChar;
+
+    private Character closingChar;
+
+    private HashMap<Character, Integer> wrapers;
+
+    private boolean rightNumOfClosings(Integer x, Integer y) {
+
+        if (x == y){
+
+             return true;
+        }
+        return false;
+    }
+
+    public boolean symbolChecker (String x){
+
+
+        wrapers = new HashMap<Character, Integer>();
+
+        wrapers.put(getOpenChar(), 0);
+        wrapers.put(getClosingChar(), 0);
+
+
+        for(int i = 0; i < x.length(); i++) {
+
+            if (wrapers.containsKey(x.charAt(i))){
+
+                wrapers.put(x.charAt(i), wrapers.get(x.charAt(i)) + 1);
+
+            }
+        }
+
+        if (getOpenChar() != getClosingChar()) {
+            return rightNumOfClosings(wrapers.get(getOpenChar()), wrapers.get(getClosingChar()));
+        } else {
+
+           return (wrapers.get(getOpenChar()) % 2 == 0);
+
+        }
+    }
+
+    public Character getOpenChar() {
+        return openChar;
+    }
+
+    public void setOpenChar(Character openChar) {
+        this.openChar = openChar;
+    }
+
+    public Character getClosingChar() {
+        return closingChar;
+    }
+
+    public void setClosingChar(Character closingChar) {
+        this.closingChar = closingChar;
+    }
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,87 @@
 package io.codedifferently;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class OpenCloseCheckerTest {
+
+    private String givenInput;
+    OpenCloseChecker testChecker;
+    @Before
+    public void setUp() {
+        testChecker = new OpenCloseChecker();
+        givenInput = "";
+
+    }
+
+    @Test
+    public void symbolCheckerTest1 () {
+
+        givenInput = "((xX))";
+        testChecker.setOpenChar('(');
+        testChecker.setClosingChar(')');
+        Assert.assertTrue("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+
+        givenInput = "[[xX]]";
+        testChecker.setOpenChar('[');
+        testChecker.setClosingChar(']');
+        Assert.assertTrue("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+
+        givenInput = "<<xX>>";
+        testChecker.setOpenChar('<');
+        testChecker.setClosingChar('>');
+        Assert.assertTrue("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+
+        givenInput = "{{xX}}";
+        testChecker.setOpenChar('{');
+        testChecker.setClosingChar('}');
+        Assert.assertTrue("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+
+        givenInput = "\"\"xX\"\"";
+        testChecker.setOpenChar('"');
+        testChecker.setClosingChar('"');
+        Assert.assertTrue("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+
+        givenInput = "\'\'xX\'\'";
+        testChecker.setOpenChar('\'');
+        testChecker.setClosingChar('\'');
+        Assert.assertTrue("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+    }
+
+    @Test
+    public void parenthesisCheckerTest2 () {
+
+        givenInput = "((xX)";
+        testChecker.setOpenChar('(');
+        testChecker.setClosingChar(')');
+        Assert.assertFalse("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+
+        givenInput = "[[xX]";
+        testChecker.setOpenChar('[');
+        testChecker.setClosingChar(']');
+        Assert.assertFalse("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+
+        givenInput = "<<xX>";
+        testChecker.setOpenChar('<');
+        testChecker.setClosingChar('>');
+        Assert.assertFalse("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+
+        givenInput = "{{xX}";
+        testChecker.setOpenChar('{');
+        testChecker.setClosingChar('}');
+        Assert.assertFalse("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+
+        givenInput = "\"\"xX\"";
+        testChecker.setOpenChar('"');
+        testChecker.setClosingChar('"');
+        Assert.assertFalse("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+
+        givenInput = "\'\'xX\'";
+        testChecker.setOpenChar('\'');
+        testChecker.setClosingChar('\'');
+        Assert.assertFalse("parenthesisChecker returns true test given input is: " + givenInput, testChecker.symbolChecker(givenInput));
+    }
+
 
 }
