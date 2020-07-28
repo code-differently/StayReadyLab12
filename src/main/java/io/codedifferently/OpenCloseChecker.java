3cd87b60913408186a5c00790c1c9ba0deff370d
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-27 20:21:08 -0500

    did the best I could

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -6,52 +6,67 @@ import java.util.logging.Logger;
 public class OpenCloseChecker {
     private Stack<Character> checker;
     private static final Logger myLogger = Logger.getLogger("io.codedifferently");
+    private int numOfSingleQuotes = 0;
+    private int numOfDoubleQuotes = 0;
 
     public OpenCloseChecker() {
         checker = new Stack<Character>();
+        numOfSingleQuotes = 0;
+        numOfDoubleQuotes = 0;
     }
 
     public static void main(String[] args) {
         OpenCloseChecker openClosed = new OpenCloseChecker();
         myLogger.info("answer is: " + openClosed.checkIfCharactersArePaired("abc\'\'"));
-
     }
 
     public boolean checkIfCharactersArePaired(String value) {
         boolean isStringStillValid = true;
-        boolean haveNotCheckedQuotation = true;
         for(Character character: value.toCharArray()) {
             if(isStringStillValid) {
-                switch(character) {
-                    case '(':
-                    case '{':
-                    case '[':
-                    case '<':
-                    case '\"':
-                    case '\'':
-                        checker.add(character);
-                        haveNotCheckedQuotation = false;
-                        break;
-                    case ')':
-                        isStringStillValid = lastCharacterWasOpeningOfSpecificType('(');
-                        break;
-                    case '}':
-                        isStringStillValid = lastCharacterWasOpeningOfSpecificType('{');
-                        break;
-                    case ']':
-                        isStringStillValid = lastCharacterWasOpeningOfSpecificType('[');
-                        break;
-                    case '>':
-                        isStringStillValid = lastCharacterWasOpeningOfSpecificType('<');
-                        break;
+                if(getNumOfSingleQuotes() == 0 && getNumOfDoubleQuotes() == 0) {
+                    switch(character) {
+                        case '(':
+                        case '{':
+                        case '[':
+                        case '<':
+                            checker.add(character);
+                            break;
+                        case ')':
+                            isStringStillValid = lastCharacterWasOpeningOfSpecificType('(');
+                            break;
+                        case '}':
+                            isStringStillValid = lastCharacterWasOpeningOfSpecificType('{');
+                            break;
+                        case ']':
+                            isStringStillValid = lastCharacterWasOpeningOfSpecificType('[');
+                            break;
+                        case '>':
+                            isStringStillValid = lastCharacterWasOpeningOfSpecificType('<');
+                            break;
+                    }
                 }
-                if(haveNotCheckedQuotation && character == '\"') {
-                    isStringStillValid = lastCharacterWasOpeningOfSpecificType('\"');
+
+                if(character == '\'') {
+                    if(getNumOfSingleQuotes() == 1) {
+                        isStringStillValid = lastCharacterWasOpeningOfSpecificType('\'');
+                        setNumOfSingleQuotes(getNumOfSingleQuotes() - 1);
+                    }
+                    else {
+                        checker.add('\'');
+                        setNumOfSingleQuotes(getNumOfSingleQuotes() + 1);
+                    }
                 }
-                else if(haveNotCheckedQuotation && character == '\'') {
-                    isStringStillValid = lastCharacterWasOpeningOfSpecificType('\'');
+                if(character == '\"') {
+                    if(getNumOfDoubleQuotes() == 1) {
+                        isStringStillValid = lastCharacterWasOpeningOfSpecificType('\"');
+                        setNumOfDoubleQuotes(getNumOfDoubleQuotes() - 1);
+                    }
+                    else {
+                        checker.add('\"');
+                        setNumOfDoubleQuotes(getNumOfDoubleQuotes() + 1);
+                    }
                 }
-                haveNotCheckedQuotation = true;
             }
             else {
                 return false;
@@ -72,5 +87,21 @@ public class OpenCloseChecker {
 
     public Stack<Character> getChecker() {
         return checker;
+    }
+
+    public int getNumOfSingleQuotes() {
+        return numOfSingleQuotes;
+    }
+
+    public void setNumOfSingleQuotes(int numOfSingleQuotes) {
+        this.numOfSingleQuotes = numOfSingleQuotes;
+    }
+
+    public int getNumOfDoubleQuotes() {
+        return numOfDoubleQuotes;
+    }
+
+    public void setNumOfDoubleQuotes(int numOfDoubleQuotes) {
+        this.numOfDoubleQuotes = numOfDoubleQuotes;
     }
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -33,11 +33,149 @@ public class OpenCloseCheckerTest {
     }
 
     @Test
+    public void bracketsAreNotPairedTest() {
+        boolean expectedAnswer = false;
+
+        String input = "a{bc}d}";
+        boolean actualAnswer = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Test
+    public void bracketsArePairedTest() {
+        boolean expectedAnswer = true;
+
+        String input = "a{}{}{{}}";
+        boolean actualAnswer = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Test
+    public void squareBracketsAreNotPairedTest() {
+        boolean expectedAnswer = false;
+
+        String input = "a[b[c]d";
+        boolean actualAnswer = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Test
+    public void squareBracketsArePairedTest() {
+        boolean expectedAnswer = true;
+
+        String input = "a[][]";
+        boolean actualAnswer = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Test
+    public void LessThanGreaterThanAreNotPairedTest() {
+        boolean expectedAnswer = false;
+
+        String input = "a<<<>>d";
+        boolean actualAnswer = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Test
+    public void LessThanGreaterThanArePairedTest() {
+        boolean expectedAnswer = true;
+
+        String input = "<>abcd<>";
+        boolean actualAnswer = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Test
+    public void singleQuotationMarksFailTest() {
+        boolean expected = false;
+        String input = "''abcre[{'";
+
+        boolean actual = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void singleQuotationMarksSuccessTest() {
+        boolean expected = true;
+        String input = "[{']]'}]";
+
+        boolean actual = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void doubleQuotationMarksFailTest() {
+        boolean expected = false;
+        String input = "\"\"\"";
+
+        boolean actual = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void doubleQuotationMarksSuccessTest() {
+        boolean expected = true;
+        String input = "\"asgdare[][]\"";
+
+        boolean actual = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
     public void getCheckerTest() {
         int expectedSize = 0;
 
         int actualSize = checker.getChecker().size();
 
         Assert.assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void getNumOfSingleQuotes() {
+        int expected = 0;
+
+        int actual = checker.getNumOfSingleQuotes();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNumOfSingleQuotes() {
+        int expected = 3;
+
+        checker.setNumOfSingleQuotes(3);
+        int actual = checker.getNumOfSingleQuotes();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNumOfDoubleQuotes() {
+        int expected = 0;
+
+        int actual = checker.getNumOfDoubleQuotes();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNumOfDoubleQuotes() {
+        int expected = 4;
+
+        checker.setNumOfDoubleQuotes(4);
+        int actual = checker.getNumOfDoubleQuotes();
+
+        Assert.assertEquals(expected, actual);
     }
 }
