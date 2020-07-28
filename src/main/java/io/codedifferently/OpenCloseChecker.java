c7e2f10f1b0aac597756a571d3337bad5b62188e
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-27 19:26:46 -0500

    almost done

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,76 @@
 package io.codedifferently;
 
+import java.util.Stack;
+import java.util.logging.Logger;
+
 public class OpenCloseChecker {
+    private Stack<Character> checker;
+    private static final Logger myLogger = Logger.getLogger("io.codedifferently");
+
+    public OpenCloseChecker() {
+        checker = new Stack<Character>();
+    }
+
+    public static void main(String[] args) {
+        OpenCloseChecker openClosed = new OpenCloseChecker();
+        myLogger.info("answer is: " + openClosed.checkIfCharactersArePaired("abc\'\'"));
+
+    }
+
+    public boolean checkIfCharactersArePaired(String value) {
+        boolean isStringStillValid = true;
+        boolean haveNotCheckedQuotation = true;
+        for(Character character: value.toCharArray()) {
+            if(isStringStillValid) {
+                switch(character) {
+                    case '(':
+                    case '{':
+                    case '[':
+                    case '<':
+                    case '\"':
+                    case '\'':
+                        checker.add(character);
+                        haveNotCheckedQuotation = false;
+                        break;
+                    case ')':
+                        isStringStillValid = lastCharacterWasOpeningOfSpecificType('(');
+                        break;
+                    case '}':
+                        isStringStillValid = lastCharacterWasOpeningOfSpecificType('{');
+                        break;
+                    case ']':
+                        isStringStillValid = lastCharacterWasOpeningOfSpecificType('[');
+                        break;
+                    case '>':
+                        isStringStillValid = lastCharacterWasOpeningOfSpecificType('<');
+                        break;
+                }
+                if(haveNotCheckedQuotation && character == '\"') {
+                    isStringStillValid = lastCharacterWasOpeningOfSpecificType('\"');
+                }
+                else if(haveNotCheckedQuotation && character == '\'') {
+                    isStringStillValid = lastCharacterWasOpeningOfSpecificType('\'');
+                }
+                haveNotCheckedQuotation = true;
+            }
+            else {
+                return false;
+            }
+        }
+        return checker.size() == 0 && isStringStillValid;
+    }
+
+    public boolean lastCharacterWasOpeningOfSpecificType(Character character) {
+        if(checker.size() != 0 && checker.peek() == character) {
+            checker.pop();
+        }
+        else {
+            return false;
+        }
+        return true;
+    }
+
+    public Stack<Character> getChecker() {
+        return checker;
+    }
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,43 @@
 package io.codedifferently;
 
-public class OpenCloseCheckerTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class OpenCloseCheckerTest {
+    OpenCloseChecker checker;
+
+    @Before
+    public void setup() {
+        checker = new OpenCloseChecker();
+    }
+
+    @Test
+    public void parenthesisAreNotPairedTest() {
+        boolean expectedAnswer = false;
+
+        String input = "a(bc(d)))";
+        boolean actualAnswer = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Test
+    public void parenthesisArePairedTest() {
+        boolean expectedAnswer = true;
+
+        String input = "a(b)c(d)e(f)g";
+        boolean actualAnswer = checker.checkIfCharactersArePaired(input);
+
+        Assert.assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Test
+    public void getCheckerTest() {
+        int expectedSize = 0;
+
+        int actualSize = checker.getChecker().size();
+
+        Assert.assertEquals(expectedSize, actualSize);
+    }
 }
