7f38da33caac87e071dd693743035a4cec8ff142
Joshua Bowman <joshuabowman@Joshuas-MacBook-Air.local>
2020-07-30 14:11:12 -0400

    work

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,57 @@
 package io.codedifferently;
 
+import java.util.Stack;
+
 public class OpenCloseChecker {
+
+    public Boolean parenthesisChecker(String input){
+        char[] string = input.toCharArray();
+
+        Stack <Character> stack= new Stack <Character>();
+        Boolean parenthesisClosed = false;
+
+        for (int i = 0; i < string.length; i++){
+            if (string[i] == ')' && stack.isEmpty() == true){
+                parenthesisClosed = false;
+                return parenthesisClosed;
+            }
+
+            if (string[i] == '('){
+                stack.push(string[i]);
+            }
+
+            if (string[i] == ')' && stack.isEmpty() == false){
+                stack.pop();
+            }
+
+            if (stack.isEmpty() == true){
+                parenthesisClosed = true;
+            }
+        }
+
+        return parenthesisClosed;
+    }
+
+    public boolean expandedChecker(String input, Character openChar, Character closedChar){
+        Stack<Character> stack = new Stack<Character>();
+        Boolean parenthesisClosed = false;
+
+        for(int i = 0; i < input.length(); i++)
+        {
+            if(input.charAt(i)== openChar && stack.contains(openChar) == true)
+            {
+                stack.pop();
+            }
+            if(input.charAt(i) == openChar && stack.contains(openChar) == false)
+            {
+                stack.push(openChar);
+            }
+        }
+        if(stack.empty() == false)
+        {
+            parenthesisClosed = true;
+        }
+
+        return parenthesisClosed;
+    }
 }
