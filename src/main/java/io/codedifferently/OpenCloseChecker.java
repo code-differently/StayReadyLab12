e8834a35a0a8fa89ca7c9a54f7a552d91571ff82
mercelisvaughan <mercelis.vaughan51@gmail.com>
2020-07-31 17:52:52 -0400

    Lab 12

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,30 @@
 package io.codedifferently;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class OpenCloseChecker {
+
+    public Boolean hasClose(String s){
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        Stack<Character> stack = new Stack<Character>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+        map.put('<', '>');
+        map.put('"', '"');
+        for (int i = 0; i < s.length(); i++){
+            char currentCharacter = s.charAt(i);
+            if (map.keySet().contains(currentCharacter)){
+                stack.push(currentCharacter);
+            } else if (map.values().contains(currentCharacter)){
+                if (!stack.empty() && map.get(stack.peek()) == currentCharacter){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,75 @@
 package io.codedifferently;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class OpenCloseCheckerTest {
+
+    @Test
+    public void hasCloseTest(){
+        // Given
+        OpenCloseChecker o = new OpenCloseChecker();
+        String s = "a(b)c(d)e(f)g";
+        // When
+        Boolean actual = o.hasClose(s);
+        // Then
+        Assert.assertEquals(false, actual);
+    }
+
+
+    @Test
+    public void hasCloseTest02(){
+        // Given
+        OpenCloseChecker o = new OpenCloseChecker();
+        String s = ")";
+        // When
+        Boolean actual = o.hasClose(s);
+        // Then
+        Assert.assertEquals(false, actual);
+    }
+
+    @Test
+    public void hasCloseTest03(){
+        // Givem
+        OpenCloseChecker o = new OpenCloseChecker();
+        String s = "a(bcd(ef))";
+        // When
+        Boolean actual = o.hasClose(s);
+        // Then
+        Assert.assertEquals(true, actual);
+    }
+
+    @Test
+    public void hasCloseTest04(){
+        // Givem
+        OpenCloseChecker o = new OpenCloseChecker();
+        String s = "a(bc(d)))";
+        // When
+        Boolean actual = o.hasClose(s);
+        // Then
+        Assert.assertEquals(false, actual);
+    }
+
+    @Test
+    public void hasCloseTest05(){
+        // Givem
+        OpenCloseChecker o = new OpenCloseChecker();
+        String s = "{(a(bc(d)))}";
+        // When
+        Boolean actual = o.hasClose(s);
+        // Then
+        Assert.assertEquals(true, actual);
+    }
+
+    @Test
+    public void hasCloseTest06(){
+        // Givem
+        OpenCloseChecker o = new OpenCloseChecker();
+        String s = "{}<>";
+        // When
+        Boolean actual = o.hasClose(s);
+        // Then
+        Assert.assertEquals(true, actual);
+    }
 
 }
