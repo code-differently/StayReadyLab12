750f58875365ea1b695caeec9273681cdcd622aa
Angela <asession001@gmail.com>
2020-08-01 16:47:49 -0400

    Finished:

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,38 @@
 package io.codedifferently;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Stack;
+
 public class OpenCloseChecker {
+
+    public boolean openNCloseChecker(String input) {
+        HashMap<Character, Character> closeBracketMap = new HashMap<Character, Character>();
+        closeBracketMap.put(')', '(');
+        closeBracketMap.put(']', '[');
+        closeBracketMap.put('}', '{');
+        closeBracketMap.put('<', '>');
+        closeBracketMap.put('"', '"');
+        HashSet<Character> openBracketSet = new HashSet<Character>(
+                closeBracketMap.values());
+        Stack<Character> stack = new Stack<Character>();
+
+        char[] chars = input.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char cur = chars[i];
+            if (openBracketSet.contains(cur)) {
+                stack.push(cur);
+            } else { // close brackets
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (closeBracketMap.get(cur) != stack.peek()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+
+    }
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,62 @@
 package io.codedifferently;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class OpenCloseCheckerTest {
+
+    @Test
+    public void openNCloseCheckerTest1(){
+        OpenCloseChecker openCloseChecker = new OpenCloseChecker();
+
+        boolean actual = openCloseChecker.openNCloseChecker("a(b)c(d)e(f)g");
+
+        Assert.assertEquals(false,actual);
+    }
+
+    @Test
+    public void openNCloseCheckerTest2(){
+        OpenCloseChecker openCloseChecker = new OpenCloseChecker();
+
+        boolean actual = openCloseChecker.openNCloseChecker(")");
+
+        Assert.assertEquals(false,actual);
+    }
+
+    @Test
+    public void openNCloseCheckerTest3(){
+        OpenCloseChecker openCloseChecker = new OpenCloseChecker();
+
+        boolean actual = openCloseChecker.openNCloseChecker("a(bcd(ef))");
+
+        Assert.assertEquals(false,actual);
+    }
+
+    @Test
+    public void openNCloseCheckerTest4(){
+        OpenCloseChecker openCloseChecker = new OpenCloseChecker();
+
+        boolean actual = openCloseChecker.openNCloseChecker("a(bc(d)))");
+
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void openNCloseCheckerTest5(){
+        OpenCloseChecker openCloseChecker = new OpenCloseChecker();
+
+        boolean actual = openCloseChecker.openNCloseChecker("h<e>l<l>0<>");
+
+        Assert.assertEquals(false,actual);
+    }
+
+    @Test
+    public void openNCloseCheckerTest6(){
+        OpenCloseChecker openCloseChecker = new OpenCloseChecker();
+
+        boolean actual = openCloseChecker.openNCloseChecker("{grebtrshythb}");
+
+        Assert.assertFalse(actual);
+    }
 
 }
