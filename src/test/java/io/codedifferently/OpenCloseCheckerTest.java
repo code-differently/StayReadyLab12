982244f1e3889f1d88a1339b7f3d9825ff92130c
Giovane Barnes <gjbarnes19@students.desu.edu@Giovanes-MacBook-Pro.local>
2020-07-31 20:25:47 -0400

    Finished Lab

--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,110 @@
 package io.codedifferently;
 
+import org.junit.Test;
+import org.junit.Assert;
 public class OpenCloseCheckerTest {
-
+    @Test
+    public void parCheck1(){
+        //Given
+        OpenCloseChecker ocp = new OpenCloseChecker();
+        String input = "a(b)c(d)e(f)g";
+        Boolean expected = true;
+        //When
+        Boolean actual = ocp.parCheck(input);
+        System.out.println(actual);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void parCheck2(){
+        //Given
+        OpenCloseChecker ocp = new OpenCloseChecker();
+        String input = ")";
+        Boolean expected = false;
+        //When
+        Boolean actual = ocp.parCheck(input);
+        System.out.println(actual);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void parCheck3(){
+        //Given
+        OpenCloseChecker ocp = new OpenCloseChecker();
+        String input = "a(bcd(ef))";
+        Boolean expected = true;
+        //When
+        Boolean actual = ocp.parCheck(input);
+        System.out.println(actual);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void parCheck4(){
+        //Given
+        OpenCloseChecker ocp = new OpenCloseChecker();
+        String input = "a(bc(d)))";
+        Boolean expected = false;
+        //When
+        Boolean actual = ocp.parCheck(input);
+        System.out.println(actual);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void specialCheck1(){
+        //Given
+        OpenCloseChecker ocp = new OpenCloseChecker();
+        String input = "a{b}c{d}e{f}g";
+        Character openChar = '{';
+        Character closedChar = '}';
+        Boolean expected = true;
+        //When
+        Boolean actual = ocp.specialChecker(input, openChar, closedChar);
+        System.out.println(actual);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void specialCheck2(){
+        //Given
+        OpenCloseChecker ocp = new OpenCloseChecker();
+        String input = "a[b]c[d]e[f]g";
+        Character openChar = '[';
+        Character closedChar = ']';
+        Boolean expected = true;
+        //When
+        Boolean actual = ocp.specialChecker(input, openChar, closedChar);
+        System.out.println(actual);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void specialCheck3(){
+        //Given
+        OpenCloseChecker ocp = new OpenCloseChecker();
+        String input = "a<b>c<d>e<f>g";
+        Character openChar = '<';
+        Character closedChar = '>';
+        Boolean expected = true;
+        //When
+        Boolean actual = ocp.specialChecker(input, openChar, closedChar);
+        System.out.println(actual);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void specialCheck4(){
+        //Given
+        OpenCloseChecker ocp = new OpenCloseChecker();
+        String input = "a(bc(d)))";
+        Character openChar = '(';
+        Character closedChar = ')';
+        Boolean expected = false;
+        //When
+        Boolean actual = ocp.specialChecker(input, openChar, closedChar);
+        System.out.println(actual);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 }
