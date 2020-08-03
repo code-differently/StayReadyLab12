e69a4c374ebfafaa8c9a68c24bdf76ddcd00fc8f
brianawilliams <williamsb16@comcast.net>
2020-08-03 14:22:56 -0400

    done

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,32 @@
 package io.codedifferently;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class OpenCloseChecker {
+
+    public boolean parenthesisChecker(String input){
+        HashMap<Character, Character> matchingPairs = new HashMap<Character, Character>();
+        Stack<Character> pairs = new Stack<Character>();
+        matchingPairs.put('(', ')');
+        matchingPairs.put('{', '}');
+        matchingPairs.put('[', ']');
+        matchingPairs.put('<', '>');
+        matchingPairs.put('"', '"');
+        matchingPairs.put('\'', '\'');
+        char [] charactersOfInput = input.toCharArray();
+        for(Character elements : charactersOfInput){
+            if(matchingPairs.containsKey(elements)){
+                pairs.push(elements);
+            }else if (matchingPairs.containsValue(elements)){
+                if(!pairs.empty() && matchingPairs.get(pairs.peek()) == elements){
+                    pairs.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return pairs.empty();
+
+    }
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,63 @@
 package io.codedifferently;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class OpenCloseCheckerTest {
+
+    @Test
+    public void parentheseCheckerTest01(){
+        //Given
+        OpenCloseChecker openCloseChecker = new OpenCloseChecker();
+        String input = "a(b)c(d)e(f)g";
+
+        //When
+        Boolean actual = openCloseChecker.parenthesisChecker(input);
+
+        //Then
+        Assert.assertTrue(input,actual);
+    }
+
+    @Test
+    public void parentheseCheckerTest02(){
+        //Given
+        OpenCloseChecker openCloseChecker = new OpenCloseChecker();
+        String input = ")";
+
+
+        //When
+        Boolean actual = openCloseChecker.parenthesisChecker(input);
+
+        //Then
+        Assert.assertFalse(input,actual);
+    }
+
+    @Test
+    public void parentheseCheckerTest03(){
+        //Given
+        OpenCloseChecker openCloseChecker = new OpenCloseChecker();
+        String input = "a(bcd(ef))";
+
+
+        //When
+        Boolean actual = openCloseChecker.parenthesisChecker(input);
+
+        //Then
+        Assert.assertTrue(input,actual);
+    }
+
+    @Test
+    public void parentheseCheckerTest04(){
+        //Given
+        OpenCloseChecker openCloseChecker = new OpenCloseChecker();
+        String input = "a(bc(d)))";
+
+
+        //When
+        Boolean actual = openCloseChecker.parenthesisChecker(input);
+
+        //Then
+        Assert.assertFalse(input,actual);
+    }
 
 }
