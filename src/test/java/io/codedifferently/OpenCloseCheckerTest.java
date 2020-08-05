b4682efb363656ef346f52eed58ff6bbaa869c20
Brandon Daughtry <bdaughtr@aggies.ncat.edu>
2020-08-05 14:53:03 -0400

    part1

--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,45 @@
 package io.codedifferently;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+
 public class OpenCloseCheckerTest {
+
+    @Test
+    public void openCloseCheckerTest1(){
+        //Given
+        String input = "a(b)c(d)e(f)g";
+
+        //Assert
+        Assert.assertTrue(OpenCloseChecker.openCloseChecker(input));
+    }
+
+    @Test
+    public void openCloseCheckerTest2(){
+        //Given
+        String input = ")";
+
+        //Assert
+        Assert.assertFalse(OpenCloseChecker.openCloseChecker(input));
+    }
+
+    @Test
+    public void openCloseCheckerTest3(){
+        //Given
+        String input = "a(bc(d)))";
+
+        //Assert
+        Assert.assertFalse(OpenCloseChecker.openCloseChecker(input));
+    }
+
+    @Test
+    public void openCloseCheckerTest4(){
+        //Given
+        String input = "a(bc(d)})";
+
+        //Assert
+        Assert.assertFalse(OpenCloseChecker.openCloseChecker(input));
+    }
 
 }
